db85f01e16cbcb2d66c047e5acca97a0b5a6d3fa
Seng Kit Mun <cellkit11@gmail.com>
2022-07-14 09:04:19 -0400

    password validator

--- src/App.js
+++ src/App.js
@@ -3,38 +3,6 @@ import Validator from './components/Validator'
 
 const App = () => {
   return <Validator />
-  // return (
-  //   <div>
-  //     <Cell />
-  //     <Cell />
-  //     <Cell />
-  //   </div>
-  // )
 }
 
 export default App
-
-// const Cell = () => {
-//   const arr = [1]
-//   const [value, setValue] = useState(arr)
-//   const onClick = () => {
-//     arr[0] = 2
-//     console.log(arr[0])
-//     console.log(arr === value)
-//   }
-//   return <button onClick={onClick}>{value[0]}</button>
-
-//   // const [value] = useState(1)
-//   // return <button>{value}</button>
-// }
-
-// const a = [1,2,3,4,5]
-
-// const one = a[0]
-// const two = a[1]
-// const three = a[2]
-// const four = a[3]
-// const five = a[4]
-
-// //
-// const [one, two, three, four, five] = a;
